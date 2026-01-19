08196de932f3252df1bde53217fb401aa31aa4ce
Allen Guo <guosonglin001@gmail.com>
2026-01-19 10:55:33 +0800

    wip

--- src/xyz/__init__.py
+++ src/xyz/__init__.py
@@ -1,3 +1,9 @@
 from __future__ import annotations
 
 from . import *  # noqa: F403
+from . import _version as version  # noqa: F401
+
+try:
+    from . import _C  # noqa: F401  # ty:ignore[unresolved-import]
+except:  # noqa: E722
+    pass
--- tests/test_csrc.py
+++ tests/test_csrc.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 import xyz
 
 
+# TODO: add a pytest marker
 def test_mul():
     x0, x1 = 2, 3
-    y = xyz.mul(x0, x1)
+    y = xyz._C.mul(x0, x1)
     z = x0 * x1
     assert y == z
